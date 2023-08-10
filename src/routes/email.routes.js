fcd37dc5c792f841e9da9e859cd8623cc44f16c5
sansito84 <santiagosito@gmail.com>
2023-08-09 21:45:35 -0300

    mailer cotiz all

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -1,12 +1,22 @@
-const express = require('express');
-const{sendEmail} = require('../controllers/mail.controllers');
+const express = require("express");
+const { sendEmail, sendEmailCoti, sendEmailCotiClient } = require("../controllers/mail.controllers");
 const router = express.Router();
 
-
-router.post('/sendEmail', (req, res) => {
-    sendEmail(req.body)
-      .then((response) => res.send(response.message))
-      .catch((error) => res.status(500).send(error.message));
-  });
+router
+    .post("/sendEmail", (req, res) => {
+        sendEmail(req.body)
+            .then((response) => res.send(response.message))
+            .catch((error) => res.status(500).send(error.message));
+    })
+    .post("/sendEmailCoti", (req, res) => {
+        sendEmailCoti(req.body)
+            .then((response) => res.send(response.message))
+            .catch((error) => res.status(500).send(error.message));
+    })
+    .post("/sendEmailCotiClient", (req, res) => {
+      sendEmailCotiClient(req.body)
+          .then((response) => res.send(response.message))
+          .catch((error) => res.status(500).send(error.message));
+  })
 
 module.exports = router;
